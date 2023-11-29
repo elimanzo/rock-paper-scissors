eab763fac2af81c1d09e044ac52025455c3a9f39
Eli Manzo <96manzo.eli@gmail.com>
2023-11-28 22:34:03 -0800

    Adding Rock Paper Scissors code

--- App.js
+++ App.js
@@ -1,20 +1,25 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, Text, View } from 'react-native';
+
+import { useState } from 'react';
 
 export default function App() {
+  const [data, setData] = useState(null);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View>
+      {data == null ? (
+        <View>
+          <Text>Rock / Paper / Scissors</Text>
+          <Button title="Rock" onPress={() => setData("Rock")} />
+          <Button title="Paper" onPress={() => setData("Paper")} />
+          <Button title="Scissors" onPress={() => setData("Scissors")} />
+        </View>
+      ) : (
+        <View>
+          <Text>You picked {data}.</Text>
+          <Button title="Reset" onPress={() => setData(null)} />
+        </View>
+      )}
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
