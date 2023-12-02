e793da9a12b93576f0556b183c6e76010b008451
Eli Manzo <96manzo.eli@gmail.com>
2023-12-02 15:01:59 -0800

    adding win conditions

--- App.js
+++ App.js
@@ -1,40 +1,86 @@
-import { Button, Text, View } from 'react-native';
+import { Button, Text, View } from 'react-native'
 
-import { useState } from 'react';
+import { useState } from 'react'
 
-const MOVES = ['Rock', 'Paper', 'Scissors'];
+// const options = ['Rock', 'Paper', 'Scissors']
+const MOVES = {
+  Rock: { weak: 'Paper', strong: 'Scissors' },
+  Paper: { weak: 'Scissors', strong: 'Rock' },
+  Scissors: { weak: 'Rock', strong: 'Paper' }
+}
+
 function getRandomArrayElement(arr) {
-  return arr[Math.floor(Math.random() * arr.length)];
+  return arr[Math.floor(Math.random() * arr.length)]
 }
 
 export default function App() {
-  const [player, setPlayer] = useState(null);
-  const [bot, setBot] = useState(null);
-  const [winLossDraws, setwinLossDraws] = useState({ wins: 0, losses: 0, draws: 0});
+  const [player, setPlayer] = useState(null)
+  const [bot, setBot] = useState(null)
+  const [score, setScore] = useState({
+    wins: 0,
+    losses: 0,
+    draws: 0
+  })
   const makeMoves = (move) => {
-    setBot(getRandomArrayElement(MOVES));
-    setPlayer(move);
-  };
-
-  const winner = () => {
-    
+    const botMove = getRandomArrayElement(Object.keys(MOVES))
+    setBot(botMove)
+    setPlayer(move)
+    whoWon(move, botMove)
   }
+
+  const whoWon = (playerMove, botMove) => {
+    // if (playerMove === botMove) {
+    //   setScore((prev) => ({ ...prev, draws: prev.draws + 1 }))
+    //   return
+    // }
+    // if (
+    //   (options.indexOf(playerMove) + 1) % options.length ===
+    //   options.indexOf(botMove)
+    // ) {
+    //   setScore((prev) => ({ ...prev, losses: prev.losses + 1 }))
+    // } else {
+    //   setScore((prev) => ({ ...prev, wins: prev.wins + 1 }))
+    // }
+
+    if (MOVES[playerMove].strong === MOVES[botMove].weak) {
+      setScore((prev) => ({ ...prev, wins: prev.wins + 1 }))
+      return
+    }
+    if (MOVES[playerMove].weak === MOVES[botMove].strong) {
+      setScore((prev) => ({ ...prev, losses: prev.losses + 1 }))
+      return
+    }
+    setScore((prev) => ({ ...prev, draws: prev.draws + 1 }))
+  }
+
   return (
-    <View style={{padding: 100}}>
+    <View style={{ padding: 100 }}>
+      <View>
+        <Text>
+          Wins: {score.wins} Losses: {score.losses} Draws: {score.draws}
+        </Text>
+      </View>
       {player == null ? (
         <View>
           <Text>Rock / Paper / Scissors</Text>
-          <Button title="Rock" onPress={() => makeMoves("Rock")} />
-          <Button title="Paper" onPress={() => makeMoves("Paper")} />
-          <Button title="Scissors" onPress={() => makeMoves("Scissors")} />
+          <Button title="Rock" onPress={() => makeMoves('Rock')} />
+          <Button title="Paper" onPress={() => makeMoves('Paper')} />
+          <Button title="Scissors" onPress={() => makeMoves('Scissors')} />
         </View>
       ) : (
         <View>
-          <Text>You picked {player}. Bot picked {bot}</Text>
-          <Button title="Reset" onPress={() => setPlayer(null)} />
+          <Text>
+            You picked {player}. Bot picked {bot}
+          </Text>
+          <Button
+            title="Reset"
+            onPress={() => {
+              setPlayer(null)
+              setBot(null)
+            }}
+          />
         </View>
       )}
     </View>
-    
-  );
+  )
 }
