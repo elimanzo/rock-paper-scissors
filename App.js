29037cf60f028dd51c6e23f235eac83c1f7a5ca7
Eli Manzo <96manzo.eli@gmail.com>
2024-01-03 00:21:34 -0800

    Increase Font, and added Read Me

--- App.js
+++ App.js
@@ -1,134 +1,141 @@
-import React from 'react'
-import { Button, Text, View, StyleSheet } from 'react-native'
-import { useState } from 'react'
+import React from 'react';
+import { Button, Text, View, StyleSheet } from 'react-native';
+import { useState } from 'react';
 
 const MOVES = {
   Rock: {
-    beats: new Set(['Scissors', 'Lizard'])
+    beats: new Set(['Scissors', 'Lizard']),
   },
   Paper: {
-    beats: new Set(['Rock', 'Spock'])
+    beats: new Set(['Rock', 'Spock']),
   },
   Scissors: {
-    beats: new Set(['Paper', 'Lizard'])
+    beats: new Set(['Paper', 'Lizard']),
   },
   Lizard: {
-    beats: new Set(['Paper', 'Spock'])
+    beats: new Set(['Paper', 'Spock']),
   },
   Spock: {
-    beats: new Set(['Scissors', 'Rock'])
-  }
-}
+    beats: new Set(['Scissors', 'Rock']),
+  },
+};
 
 const getRandomArrayElement = (arr) => {
-  return arr[Math.floor(Math.random() * arr.length)]
-}
+  return arr[Math.floor(Math.random() * arr.length)];
+};
 
 export default function App() {
-  const [player, setPlayer] = useState(null)
-  const [bot, setBot] = useState(null)
-  const [stateResult, setStateResult] = useState(null)
+  const [player, setPlayer] = useState(null);
+  const [bot, setBot] = useState(null);
+  const [stateResult, setStateResult] = useState(null);
   const [score, setScore] = useState({
     wins: 0,
     losses: 0,
-    draws: 0
-  })
+    draws: 0,
+  });
 
   const makeMoves = (move) => {
-    const botMove = getRandomArrayElement(Object.keys(MOVES))
-    setBot(botMove)
-    setPlayer(move)
-    whoWon(move, botMove)
-  }
+    const botMove = getRandomArrayElement(Object.keys(MOVES));
+    setBot(botMove);
+    setPlayer(move);
+    whoWon(move, botMove);
+  };
 
   const whoWon = (playerMove, botMove) => {
     if (MOVES[playerMove].beats.has(botMove)) {
-      setScore((prev) => ({ ...prev, wins: prev.wins + 1 }))
-      setStateResult('Player Wins!')
-      return
+      setScore((prev) => ({ ...prev, wins: prev.wins + 1 }));
+      setStateResult('Player Wins!');
+      return;
     }
     if (MOVES[botMove].beats.has(playerMove)) {
-      setScore((prev) => ({ ...prev, losses: prev.losses + 1 }))
-      setStateResult('Bot Wins')
-      return
+      setScore((prev) => ({ ...prev, losses: prev.losses + 1 }));
+      setStateResult('Bot Wins');
+      return;
     }
-    setScore((prev) => ({ ...prev, draws: prev.draws + 1 }))
-    setStateResult("It's a Tie!")
-  }
+    setScore((prev) => ({ ...prev, draws: prev.draws + 1 }));
+    setStateResult("It's a Tie!");
+  };
 
   return (
     <View style={styles.container}>
       <View style={styles.scoreText}>
-        <Text>
+        <Text style={styles.text}>
           Wins: {score.wins} Losses: {score.losses} Draws: {score.draws}
         </Text>
       </View>
       {player == null ? (
         <View style={styles.buttonContainer}>
-          <Text>Rock / Paper / Scissors / Lizard / Spock</Text>
+          <Text style={styles.text}>
+            Rock / Paper / Scissors / Lizard / Spock
+          </Text>
           <View style={styles.buttonRow}>
             <View style={styles.button}>
-              <Button title="Rock" onPress={() => makeMoves('Rock')} />
+              <Button title='🪨 Rock' onPress={() => makeMoves('Rock')} />
             </View>
             <View style={styles.button}>
-              <Button title="Paper" onPress={() => makeMoves('Paper')} />
+              <Button title='🧻 Paper' onPress={() => makeMoves('Paper')} />
             </View>
             <View style={styles.button}>
-              <Button title="Scissors" onPress={() => makeMoves('Scissors')} />
+              <Button
+                title='✂️ Scissors'
+                onPress={() => makeMoves('Scissors')}
+              />
             </View>
             <View style={styles.button}>
-              <Button title="Lizard" onPress={() => makeMoves('Lizard')} />
+              <Button title='🦎 Lizard' onPress={() => makeMoves('Lizard')} />
             </View>
             <View style={styles.button}>
-              <Button title="Spock" onPress={() => makeMoves('Spock')} />
+              <Button title='🖖 Spock' onPress={() => makeMoves('Spock')} />
             </View>
           </View>
         </View>
       ) : (
         <View style={styles.buttonContainer}>
-          <Text style={styles.resultText}>
+          <Text style={styles.text}>
             You picked {player}. Bot picked {bot}
           </Text>
-          <Text>{stateResult}</Text>
+          <Text style={styles.text}>{stateResult}</Text>
           <View style={styles.button}>
             <Button
-              title="Reset"
+              title='Reset'
               onPress={() => {
-                setPlayer(null)
-                setBot(null)
-                setStateResult(null)
+                setPlayer(null);
+                setBot(null);
+                setStateResult(null);
               }}
             />
           </View>
         </View>
       )}
     </View>
-  )
+  );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
-    alignItems: 'center'
+    alignItems: 'center',
   },
   scoreText: {
-    marginBottom: 20
+    fontSize: 20,
+    marginBottom: 20,
   },
   buttonContainer: {
-    alignItems: 'center'
+    alignItems: 'center',
   },
   buttonRow: {
     flexDirection: 'column',
     justifyContent: 'center',
     alignItems: 'center',
     width: '60%',
-    marginTop: 10
+    marginTop: 10,
   },
   button: {
-    marginTop: 10
+    marginTop: 10,
   },
-  resultText: {
-    marginTop: 10
-  }
-})
+  text: {
+    fontSize: 20,
+    marginTop: 10,
+  },
+});
---
+++ README.md
@@ -0,0 +1,4 @@
+# Rock-Paper-Scissors-Lizard-Spock
+
+![Rock-Paper-Scissors-Lizard-Spock](https://i.gyazo.com/fbc185892bf0a098ec98aa72bb504c4a.png)
+![Result Screen](https://i.gyazo.com/13490df6e7406ac1028ca33de99b5171.png)
